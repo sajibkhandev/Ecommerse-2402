b8d231515ca5046f9d29f20f61e17e4f83e60077
ramjanmia110 <mdramjanmia112@gmail.com>
2025-02-14 22:11:56 +0600

    error section done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Container from '../components/Container'
+import Image from '../components/Image'
+import error from '../assets/error.png'
+import { IoSearchOutline } from "react-icons/io5";
+import Button from '../components/Button';
 
 const Error = () => {
   return (
     <div>
-        <h1 className='text-5xl'>this is error Page</h1>
+        <section className='pt-[81px] pb-[140px]'>
+          <Container>
+            <div className='ml-[157px]'>
+            <Image src={error}/>
+             <p className='text-secondary font-dm font-[400] text-[16px] leading-[30px] w-[644px] pt-[49px] pb-[50px]'>The page you were looking for couldn't be found. The page could be removed or you misspelled the word while searching for it. Maybe try a search?</p>
+             <div className='w-[643px] relative pb-[60px]'>
+              <input className='w-full p-[21px] font-dm font-[400] text-[16px] leading-[30px] bg-[#F0F0F0] border-none outline-none' type="text" placeholder='Type to search'/>
+              <IoSearchOutline className='absolute top-[27px] right-[22px] w-[16px] text-primary cursor-pointer' />
+             </div>
+             <Button text='Back to Home' className='w-[200px] text-[14px] font-dm font-[700] py-[16px] px-[50px]'/>
+            </div>
+          </Container>
+        </section>
     </div>
   )
 }
