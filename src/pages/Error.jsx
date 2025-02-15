7763249232a405cf4ce4af26c6f821723988dd67
ramjanmia110 <mdramjanmia112@gmail.com>
2025-02-15 21:53:19 +0600

    error section correction

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -17,7 +17,7 @@ const Error = () => {
               <input className='w-full p-[21px] font-dm font-[400] text-[16px] leading-[30px] bg-[#F0F0F0] border-none outline-none' type="text" placeholder='Type to search'/>
               <IoSearchOutline className='absolute top-[27px] right-[22px] w-[16px] text-primary cursor-pointer' />
              </div>
-             <Button text='Back to Home' className='w-[200px] text-[14px] font-dm font-[700] py-[16px] px-[50px]'/>
+             <Button text='Back to Home' className='w-[220px] text-[14px] font-dm font-[700] py-[16px] px-[50px]'/>
             </div>
           </Container>
         </section>
