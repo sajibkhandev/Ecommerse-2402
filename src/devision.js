a799edc6aafa572064f9a1a33a5be8fc6aef6ac5
sajibkhandev <sajibkhan.mern@gmail.com>
2025-03-13 19:59:57 +0600

    api added

---
+++ src/devision.js
@@ -0,0 +1,43 @@
+let devision=[
+    {
+        devision:"Dhaka",
+        district:[
+            {
+                name:"narsingdi",
+                
+            },
+            {
+                name:"gajipur",
+                
+            },
+        ]
+    },
+    {
+        devision:"Syhlet",
+        district:[
+            {
+                name:"hobigong",
+                
+            },
+            {
+                name:"muolobibazer",
+                
+            },
+        ]
+    },
+    {
+        devision:"Rangpur",
+        district:[
+            {
+                name:"gaybandaya",
+                
+            },
+            {
+                name:"dinazpur",
+                
+            },
+        ]
+    },
+]
+
+export default devision
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,38 +1,41 @@
-import { useDispatch, useSelector } from "react-redux";
-import Container from "../components/Container";
-import CartCard from "../components/CartCard";
-import React, { useEffect, useState } from "react";
-import { counter } from "../slices/counterSlice";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import Flex from "../components/Flex";
+import { useState } from 'react';
+import Container from '../components/Container'
+import Devision from '../devision';
 
 const About = () => {
-  let [allProduct, setAllProduct] = useState([]);
+  let [devision,setDevision]=useState(Devision)
+  
 
-  useEffect(() => {
-    async function allData() {
-      let data = await axios.get("https://dummyjson.com/products");
-      setAllProduct(data.data.products);
-    }
+ 
 
-    allData();
-  }, []);
-
-  allProduct.map((item) => {
-    console.log(item);
-  });
+ 
   return (
     <Container className="py-10">
-      <Flex className=" flex-wrap justify-between">
-        {allProduct.map((item) => (
-          <CartCard
-            title={item.title}
-            price={item.price}
-            image={item.thumbnail}
-          />
-        ))}
-      </Flex>
+
+      <select >
+        <option >Select Devision</option>
+        {
+          devision.map(item=>(
+            <option >{item.devision}</option>
+          ))
+
+        }
+        
+      </select>
+     
+      <select >
+        <option >Select Distrct</option>
+        {
+          devision.map(item=>(
+            <option >{item.devision}</option>
+          ))
+
+        }
+
+       
+      </select>
+     
+     
     </Container>
   );
 };
